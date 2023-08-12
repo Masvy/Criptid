07eb71048d71dbaf6aabaddc4da560e7ba8fde94
Masvy <masvy.435@gmail.com>
2023-08-12 16:35:13 +0300

    feat: create 2 keyboards
    create keyboards(__init__.py, inline_start.py) folder
    
    in inline_start.py create 2 keyboards objects

---
+++ tgbot/keyboards/inline_start.py
@@ -0,0 +1,39 @@
+from aiogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+
+# Объект клавиатуру, в котором находятся объекты кнопок
+language_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Русский 🇷🇺',
+                                 callback_data='russian_pressed'),
+            InlineKeyboardButton(text='English 🇬🇧',
+                                 callback_data='english_pressed')
+        ]
+    ]
+)
+
+# Объект клавиатуру, в котором находятся объекты кнопок
+main_buttons_kb: InlineKeyboardMarkup = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [
+            InlineKeyboardButton(text='Криптовалюты 🪙',
+                                 callback_data='cryptocurrencies_pressed'),
+            InlineKeyboardButton(text='Аккаунт 🪪',
+                                 callback_data='account_pressed')
+        ],
+        [
+            InlineKeyboardButton(text='Резервы сервиса 💰',
+                                 callback_data='reserves_pressed'),
+            InlineKeyboardButton(text='История обмена 🗃️',
+                                 callback_data='history_pressed')
+        ],
+        [InlineKeyboardButton(text='Прогноз от Chat GPT 📈',
+                              callback_data='forecast_pressed')],
+        [
+            InlineKeyboardButton(text='Контакты 📱',
+                                 callback_data='contacts_pressed'),
+            InlineKeyboardButton(text='Отзывы 📢',
+                                 callback_data='reviews_pressed')
+        ]
+    ]
+)
